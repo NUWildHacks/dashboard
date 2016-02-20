def6094253caf13afe7b3f4dc4b1afedb0a5be3a
samirjoshi <samirjoshi2018@u.northwestern.edu>
2016-02-20 15:58:06 -0600

    index, example changed

--- example.js
+++ example.js
@@ -35,44 +35,8 @@ class MyTable extends React.Component {
           )}
           width={200}
         />
-      </Table>
+      </Table>,
+      document.getElementById('container')
     );
   }
 }
-// const rows = [
-//   ['a1', 'b1', 'c1'],
-//   ['a2', 'b2', 'c2'],
-//   ['a3', 'b3', 'c3'],
-//   // .... and more
-// ];
-//
-// // RENDERING
-// ReactDOM.render(
-//   <Table
-//   	rowHeight = {50}
-//   	num_rows = {rows.length}
-//   	width = {5000}
-//   	height = {5000}
-//   	headerHeight = {50}>
-//   	<Column
-//       header = {<Cell> Col 1</Cell}
-//       cell - {<cell> Column 1 static content </Cell>}
-//       width = {2000}
-//     />
-//     <Column
-//       header={<Cell>Col 2</Cell>}
-//       cell={<MyCustomCell mySpecialProp="column2" />}
-//       width={1000}
-//     />
-//     <Column
-//       header={<Cell>Col 3</Cell>}
-//       cell={({rowIndex, ...props}) => (
-//         <Cell {...props}>
-//           Data for column 3: {rows[rowIndex][2]}
-//         </Cell>
-//       )}
-//       width={2000}
-//     />
-//   </Table>,
-//   document.getElementById('example')
-// );
